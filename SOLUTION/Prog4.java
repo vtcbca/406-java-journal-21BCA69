ddc76e8ca04df440f69b6b0261b4461d6fa987e1
21BCA69 <96609239+21BCA69@users.noreply.github.com>
2023-04-01 17:40:46 +0530

    Solution 4

---
+++ SOLUTION/Prog4.java
@@ -0,0 +1,19 @@
+class OverloadMethod
+
+{
+ public void display(String s1,String s2)
+ {
+ System.out.println("The Concated String is:"+(s1+s2));
+ }
+ public void display(int a,int b)
+ {
+ System.out.println("The Addition of "+a+" and "+b+" is:"+(a+b));
+ }
+ public static void main(String args[])
+ {
+ OverloadMethod o=new OverloadMethod();
+ o.display("Hello"," World");
+ o.display(10,20); 
+ }
+  
+}
